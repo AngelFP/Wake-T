8f3089bd7c5cdcbf2b8ebfd851b0b175a1906a00
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2019-09-06 17:30:35 +0200

    Increase length of progress bar

--- wake_t/utilities/other.py
+++ wake_t/utilities/other.py
@@ -4,8 +4,8 @@ import sys
 
 
 def print_progress_bar(pre_string, step, total_steps):
-    n_dash = int(round(step/total_steps*10))
-    n_space = 10 - n_dash
+    n_dash = int(round(step/total_steps*20))
+    n_space = 20 - n_dash
     status = pre_string + '[' + '-'*n_dash + ' '*n_space + '] '
     if step < total_steps:
         status += '\r'
