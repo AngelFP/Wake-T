50e64a3fd78104372adc9327bf94c35565dcec9c
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2020-09-01 14:53:25 +0200

    Update example

--- examples/track_plasma_fluid.py
+++ examples/track_plasma_fluid.py
@@ -1,3 +1,9 @@
+"""
+Basic example demonstrating how to simulate a plasma stage in Wake-T using
+the 1d cold fluid wakefield model (well suited for linear to weakly nonlinear
+regimes).
+
+"""
 
 import matplotlib.pyplot as plt
 from aptools.plotting.quick_diagnostics import slice_analysis
@@ -7,10 +13,12 @@ from wake_t.driver_witness import LaserPulse
 from wake_t.utilities.bunch_generation import get_matched_bunch
 from wake_t.data_analysis import analyze_bunch_list
 
-# create laser driver
+
+# Create laser driver.
 laser = LaserPulse(100e-6, l_0=800e-9, w_0=70e-6, a_0=0.8, tau=30e-15)
 
-# create bunch (matched to a focusing srength of 1.5e5 T)
+
+# Create bunch (matched to a focusing strength of 0.13 MT/m).
 en = 0.3e-6  # m
 ene = 200  # units of beta*gamma
 ene_sp = 0.3  # %
@@ -19,21 +27,25 @@ s_t = 1  # fs
 q_tot = 1  # pC
 n_part = 1e4
 bunch = get_matched_bunch(en, en, ene, ene_sp, s_t, xi_c, q_tot, n_part,
-                          k_x=130000)
+                          k_x=0.13e6)
 
-# create plasma stage
+
+# Create plasma stage.
 plasma = PlasmaStage(
-    1e-2, 1e23, driver=laser, wakefield_model='cold_fluid_1d', n_out=20,
+    1e-2, 1e23, laser=laser, wakefield_model='cold_fluid_1d', n_out=20,
     laser_evolution=True, laser_z_foc=0, r_max=70e-6,  xi_min=40e-6,
     xi_max=120e-6, n_r=70, n_xi=50)
 
-# start tracking
+
+# Do tracking.
 bunch_list = plasma.track(bunch, out_initial=True)
 
-# analyze bunch evolution
+
+# Analyze bunch evolution.
 params_evolution = analyze_bunch_list(bunch_list)
 
-# quick plot of results
+
+# Quick plot of results.
 z = params_evolution['prop_dist'] * 1e2
 fig_1 = plt.figure()
 plt.subplot(411)
@@ -53,7 +65,6 @@ plt.plot(z, params_evolution['avg_ene'])
 plt.xlabel("z [mm]")
 plt.ylabel("$\\gamma$")
 plt.tight_layout()
-
 fig_2 = plt.figure()
 slice_analysis(bunch.x, bunch.y, bunch.xi, bunch.px, bunch.py, bunch.pz,
                bunch.q, fig=fig_2)
