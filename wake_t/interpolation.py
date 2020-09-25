772f30177514d2126a8b59df717084b1aad77c7e
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2020-09-25 15:33:12 +0200

    Add method to interpolate single field

--- wake_t/interpolation.py
+++ wake_t/interpolation.py
@@ -4,7 +4,49 @@ from numba import njit, prange
 
 
 @njit()
-def interpolate_cyl_linear(wx, ez, z_fld, r_fld, x, y, z):
+def interpolate_field_cyl_linear(fld, z_fld, r_fld, x, y, z):
+    n_part = x.shape[0]
+    fld_part = np.zeros(n_part)
+    dr = r_fld[1] - r_fld[0]
+    dz = z_fld[1] - z_fld[0]
+    inv_dr = 1./dr
+    inv_dz = 1./dz
+    z_min = z_fld[0]
+    for i in prange(n_part):
+        x_i = x[i]
+        y_i = y[i]
+        z_i = z[i]
+
+        r_i = math.sqrt(x_i**2 + y_i**2)
+        r_i_cell = r_i*inv_dr - 0.5
+        z_i_cell = (z_i - z_min)*inv_dz - 0.5
+        ir_lower = int(math.floor(r_i_cell))
+        ir_upper = ir_lower + 1
+        iz_lower = int(math.floor(z_i_cell))
+        iz_upper = iz_lower + 1
+        if ir_lower < 0:
+            ir_lower = 0
+
+        fld_ll = fld[iz_lower, ir_lower]
+        fld_lu = fld[iz_lower, ir_upper]
+        fld_ul = fld[iz_upper, ir_lower]
+        fld_uu = fld[iz_upper, ir_upper]
+
+        # Interpolate in z
+        dz_u = iz_upper - z_i_cell
+        dz_l = z_i_cell - iz_lower
+        fld_z_1 = dz_u*fld_ll + dz_l*fld_ul
+        fld_z_2 = dz_u*fld_lu + dz_l*fld_uu
+
+        # Interpolate in r
+        dr_u = ir_upper - r_i_cell
+        dr_l = 1 - dr_u        
+        fld_part[i] = dr_u*fld_z_1 + dr_l*fld_z_2
+    return fld_part
+
+
+@njit()
+def interpolate_main_fields_cyl_linear(wx, ez, z_fld, r_fld, x, y, z):
     n_part = x.shape[0]
     wx_part = np.zeros(n_part)
     wy_part = np.zeros(n_part)
