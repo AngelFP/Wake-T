5d64f9238f61e883047f37ab41a5782e4966c290
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-03-25 21:14:08 +0100

    Update import

--- wake_t/__init__.py
+++ wake_t/__init__.py
@@ -4,10 +4,10 @@ __version__ = '0.5.0'
 from .beamline_elements import (PlasmaStage, PlasmaRamp, ActivePlasmaLens,
                                 Drift, Dipole, Quadrupole, Sextupole, Beamline)
 from .physics_models.collective_effects.csr import set_csr_settings
-from .physics_models.laser.laser_pulse import LaserPulse
+from .physics_models.laser.laser_pulse import GaussianPulse
 from .particles.particle_bunch import ParticleBunch
 
 
 __all__ = ['__version__', 'PlasmaStage', 'PlasmaRamp', 'ActivePlasmaLens',
            'Drift', 'Dipole', 'Quadrupole', 'Sextupole', 'Beamline',
-           'set_csr_settings', 'LaserPulse', 'ParticleBunch']
+           'set_csr_settings', 'GaussianPulse', 'ParticleBunch']
