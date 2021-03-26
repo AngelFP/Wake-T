fbdc202fbf1f5c41fc1e76e68b218c0afe932bfe
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-03-26 13:27:09 +0100

    Adapt Wakefield class to envelope solver

--- wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis/wakefield.py
+++ wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis/wakefield.py
@@ -11,14 +11,13 @@ from wake_t.physics_models.plasma_wakefields.base_wakefield import Wakefield
 class Quasistatic2DWakefield(Wakefield):
 
     def __init__(self, density_function, laser=None, laser_evolution=False,
-                 laser_z_foc=0, r_max=None, xi_min=None, xi_max=None, n_r=100,
+                 r_max=None, xi_min=None, xi_max=None, n_r=100,
                  n_xi=100, ppc=2, dz_fields=0, p_shape='linear'):
         super().__init__()
         self.openpmd_diag_supported = True
         self.density_function = density_function
         self.laser = laser
         self.laser_evolution = laser_evolution
-        self.laser_z_foc = laser_z_foc
         self.r_max = r_max
         self.xi_min = xi_min
         self.xi_max = xi_max
@@ -61,31 +60,42 @@ class Quasistatic2DWakefield(Wakefield):
         z_beam = t*ct.c + np.average(xi)  # z postion of beam center
         n_p = self.density_function(z_beam)
 
-        # calculate distance to laser focus
-        if self.laser_evolution:
-            dz_foc = self.laser_z_foc - ct.c*t
-        else:
-            dz_foc = 0
+        # Evolve laser envelope
+        if t == 0.:
+            self.laser.set_envelope_solver_params(
+                self.xi_min, self.xi_max, self.r_max, self.n_xi, self.n_r,
+                self.dz_fields/ct.c, n_p)
+            self.laser.initialize_envelope()
+        elif self.laser_evolution:
+            self.laser.evolve(self.chi)
 
-        flds = calculate_wakefields(
-            self.laser, [x, y, xi, q], self.r_max, self.xi_min, self.xi_max,
-            self.n_r, self.n_xi, self.ppc, n_p, dz_foc, p_shape=self.p_shape)
-        n_p_mesh, W_r, E_z, psi_mesh, xi_arr, r_arr = flds
+        # Laser envelope
+        a_env = np.abs(self.laser.get_envelope()) ** 2
+
+        # Calculate plasma wakefields
+        rho, chi, W_r, E_z, xi_arr, r_arr = calculate_wakefields(
+            a_env, [x, y, xi, q], self.r_max, self.xi_min, self.xi_max,
+            self.n_r, self.n_xi, self.ppc, n_p, p_shape=self.p_shape)
 
         E_0 = ge.plasma_cold_non_relativisct_wave_breaking_field(n_p*1e-6)
         s_d = ge.plasma_skin_depth(n_p*1e-6)
 
-        self.rho = n_p_mesh.T
-        self.E_z = E_z.T*E_0
-        self.W_x = W_r.T*E_0
+        self.rho = rho
+        self.chi = chi
+        self.E_z = E_z*E_0
+        self.W_x = W_r*E_0
         self.xi_fld = xi_arr*s_d
         self.r_fld = r_arr*s_d
 
     def __interpolate_fields_to_particles(self, x, y, xi, t):
         if (self.current_t_interp is None) or (self.current_t_interp != t):
             self.current_t_interp = t
+            # Gather fields
+            dr = self.r_fld[1] - self.r_fld[0]
+            dxi = self.xi_fld[1] - self.xi_fld[0]
             interp_flds = gather_main_fields_cyl_linear(
-                self.W_x, self.E_z, self.xi_fld, self.r_fld, x, y, xi)
+                self.W_x, self.E_z, self.xi_fld[0], self.xi_fld[-1],
+                self.r_fld[0], self.r_fld[-1], dxi, dr, x, y, xi)
             self.wx_part, self.wy_part, self.ez_part = interp_flds
 
     def _get_openpmd_diagnostics_data(self):
@@ -107,7 +117,7 @@ class Quasistatic2DWakefield(Wakefield):
         fld_position = [0.5, 0.5]
         fld_names = ['E', 'W', 'rho']
         fld_comps = [['z'], ['r'], None]
-        fld_arrays = [[self.E_z.T], [self.W_x.T], [self.rho.T]]
+        fld_arrays = [[self.E_z.T[2:-2, 2:-2]], [self.W_x.T[2:-2, 2:-2]], [self.rho.T[2:-2, 2:-2]]]
         fld_comp_pos = [fld_position] * len(fld_names)
 
         # Generate dictionary for openPMD diagnostics.
