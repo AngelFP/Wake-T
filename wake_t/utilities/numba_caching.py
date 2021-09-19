d5ae9e64414836124987ce640bb3beb4ae0a462c
delaossa <alberto.martinez.de.la.ossa@desy.de>
2021-09-19 23:38:45 +0200

    typo in comment

--- wake_t/utilities/numba_caching.py
+++ wake_t/utilities/numba_caching.py
@@ -2,7 +2,7 @@ import os
 from numba import njit
 
 # Check if the environment variable WAKET_DISABLE_CACHING is set to 1
-# and in that case, disable threading
+# and in that case, disable caching
 caching = True
 if 'WAKET_DISABLE_CACHING' in os.environ:
     if int(os.environ['WAKET_DISABLE_CACHING']) == 1:
