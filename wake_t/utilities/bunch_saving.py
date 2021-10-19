f8f02253463a45deda482c0a5f4f65fc5a843f8a
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-10-19 19:34:40 +0200

    Allow saving bunch with its own (or given) name

--- wake_t/utilities/bunch_saving.py
+++ wake_t/utilities/bunch_saving.py
@@ -4,8 +4,11 @@ import aptools.data_handling.saving as ds
 
 
 def save_bunch_to_file(bunch, code_name, folder_path, file_name,
-                       reposition=False, avg_pos=[None, None, None],
-                       n_part=None):
+                       species_name=None, reposition=False,
+                       avg_pos=[None, None, None], n_part=None):
     bunch_data = bunch.get_bunch_matrix()
+    if species_name is None:
+        species_name = bunch.name
     ds.save_beam(code_name, bunch_data, folder_path, file_name,
-                 reposition=reposition, avg_pos=avg_pos, n_part=n_part)
+                 reposition=reposition, avg_pos=avg_pos, n_part=n_part,
+                 species_name=species_name)
