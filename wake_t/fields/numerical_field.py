25a1a8b26fee4bfa1192b9ebfbbe1572f47d4f33
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-04-22 20:02:40 +0200

    Implement base class for all numerical fields

---
+++ wake_t/fields/numerical_field.py
@@ -0,0 +1,57 @@
+"""Contains the base class for all numerical fields."""
+
+from .base import Field
+
+
+class NumericalField(Field):
+    """Base class for all fields that are computed numerically.
+
+    All numerical fields must have a time step `dt_update` which determines how
+    often they will be updated (i.e., recalculated). Every time `update` is
+    called, the internal time is advanced by one step and the field is
+    recalculated. The class also provides methods for initializing and evolving
+    field properties (e.g., a laser pulse envelope).
+
+    Each subclass must implement the logic for calculating and gathering the
+    field and, optionally, for initializing and evolving any field properties.
+    """
+
+    def __init__(self, dt_update):
+        """Initialize the field.
+
+        Parameters
+        ----------
+        dt_update : float
+            Update period (in seconds) of the field.
+        """
+        super().__init__()
+        self.dt_update = dt_update
+        self.initialized = False
+
+    def _update(self, t, bunches):
+        if not self.initialized:
+            self.initialize_properties(bunches)
+        else:
+            self.evolve_properties(bunches)
+        self.calculate_field(bunches)
+        
+    def initialize_properties(self, bunches):
+        self.t = 0.
+        self._initialize_properties(bunches)
+        self.initialized = True
+
+    def evolve_properties(self, bunches):
+        self.t += self.dt_update
+        self._evolve_properties(bunches)
+
+    def calculate_field(self, bunches):
+        self._calculate_field(bunches)
+
+    def _initialize_properties(self, bunches):
+        pass
+
+    def _evolve_properties(self, bunches):
+        pass
+
+    def _calculate_field(self, bunches):
+        raise NotImplementedError
