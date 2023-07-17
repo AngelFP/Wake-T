a399d32c39a153d1ba059e97fae2395a2f890379
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-07-17 16:18:00 +0200

    Split gathering of laser and bunch sources

--- wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/gather.py
+++ wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/gather.py
@@ -4,21 +4,19 @@ from wake_t.utilities.numba import njit_serial
 
 
 @njit_serial(fastmath=True)
-def gather_sources(fld_1, fld_2, fld_3, r_min, r_max, dr, r,
-                   fld_1_pp, fld_2_pp, fld_3_pp):
+def gather_laser_sources(a2, nabla_a, r_min, r_max, dr, r, a2_pp, nabla_a_pp):
     """
-    Convenient method for gathering at once the three source fields needed
-    by the Baxevanis wakefield model (a2 and nabla_a from the laser, and
-    b_theta_0 from the beam) into the plasma particles. This method is also
-    catered to cylindrical geometry and assumes all plasma particles have the
-    same longitudinal position (which is the case in a quasistatic model,
+    Gather the laser source terms (a2 and nabla_a) needed
+    by the Baxevanis wakefield model into the plasma particles. This method is
+    also catered to cylindrical geometry and assumes all plasma particles have
+    the same longitudinal position (which is the case in a quasistatic model,
     where there is only a single column of particles).
 
     Parameters
     ----------
-    fld_1, fld_2, fld_3 : ndarray
-        The three source fields, corresponding respectively to a2, nabla_a2
-        and b_theta_0. Each of them is a (nr+4) array, including 2 guard
+    a2, nabla_a : ndarray
+        The source fields, corresponding respectively to a2 and nabla_a2.
+        Each of them is a (nr+4) array, including 2 guard
         cells in each boundary.
     r_min, r_max : float
         Position of the first and last field values along r.
@@ -26,6 +24,8 @@ def gather_sources(fld_1, fld_2, fld_3, r_min, r_max, dr, r,
         Grid step size along the radial direction.
     r : 1darray
         Transverse position of the plasma particles.
+    a2_pp, nabla_a_pp : ndarray
+        Arrays where the gathered sources will be stored.
 
     """
 
@@ -53,57 +53,44 @@ def gather_sources(fld_1, fld_2, fld_3, r_min, r_max, dr, r,
                 sign = -1
 
             # Get field value at each bounding cell.
-            fld_1_l = fld_1[ir_lower]
-            fld_1_u = fld_1[ir_upper]
-            fld_2_l = fld_2[ir_lower] * sign
-            fld_2_u = fld_2[ir_upper]
-            fld_3_l = fld_3[ir_lower] * sign
-            fld_3_u = fld_3[ir_upper]
+            fld_1_l = a2[ir_lower]
+            fld_1_u = a2[ir_upper]
+            fld_2_l = nabla_a[ir_lower] * sign
+            fld_2_u = nabla_a[ir_upper]
 
             # Interpolate in r
             dr_u = ir_upper - r_i_cell
             dr_l = 1 - dr_u
-            fld_1_pp[i] = dr_u*fld_1_l + dr_l*fld_1_u
-            fld_2_pp[i] = dr_u*fld_2_l + dr_l*fld_2_u
-            fld_3_pp[i] = dr_u*fld_3_l + dr_l*fld_3_u
+            a2_pp[i] = dr_u*fld_1_l + dr_l*fld_1_u
+            nabla_a_pp[i] = dr_u*fld_2_l + dr_l*fld_2_u
         else:
-            fld_1_pp[i] = 0.
-            fld_2_pp[i] = 0.
-            fld_3_pp[i] = fld_3[-3] * r_max / r_i
+            a2_pp[i] = 0.
+            nabla_a_pp[i] = 0.
 
 
-
-@njit_serial()
-def gather_psi_bg(sum_1_bg_grid, r_min, r_max, dr, r, sum_1_bg):
+@njit_serial(fastmath=True)
+def gather_bunch_sources(b_t, r_min, r_max, dr, r, b_t_pp):
     """
-    Convenient method for gathering at once the three source fields needed
-    by the Baxevanis wakefield model (a2 and nabla_a from the laser, and
-    b_theta_0 from the beam) into the plasma particles. This method is also
+    Gathering the beam source terms (B_theta) needed by the Baxevanis
+    wakefield model. This method is also
     catered to cylindrical geometry and assumes all plasma particles have the
     same longitudinal position (which is the case in a quasistatic model,
     where there is only a single column of particles).
 
     Parameters
     ----------
-    fld_1, fld_2, fld_3 : ndarray
-        The three source fields, corresponding respectively to a2, nabla_a2
-        and b_theta_0. Each of them is a (nr+4) array, including 2 guard
+    b_t : ndarray
+        The source term corresponding to the azimuthal magnetic field of a
+        particle bunch. Array of size (nr+4) array, including 2 guard
         cells in each boundary.
-    z_min, z_max : float
-        Position of the first and last field values along z.
     r_min, r_max : float
         Position of the first and last field values along r.
-    dz, dr : float
-        Grid step size along the longitudinal and radial direction.
+    dz : float
+        Grid step size along the radial direction.
     r : 1darray
         Transverse position of the plasma particles.
-    z : int
-        Longitudinal position of the column of plasma particles.
-
-    Returns
-    --------
-    A tuple with three 1darray containing the gathered field values at the
-    position of each particle.
+    b_t_pp : ndarray
+        Array where the gathered source will be stored.
 
     """
 
@@ -116,66 +103,7 @@ def gather_psi_bg(sum_1_bg_grid, r_min, r_max, dr, r, sum_1_bg):
         # Gather field only if particle is within field boundaries.
         if r_i <= r_max:
             # Position in cell units.
-            r_i_cell = (r_i - r_min)/dr + 2
-
-            # Indices of upper and lower cells in r and z.
-            ir_lower = int(math.floor(r_i_cell))
-            ir_upper = ir_lower + 1
-
-            # Get field value at each bounding cell.
-            sum_1_bg_grid_l = sum_1_bg_grid[ir_lower]
-            sum_1_bg_grid_u = sum_1_bg_grid[ir_upper]
-
-            # Interpolate in r.
-            dr_u = ir_upper - r_i_cell
-            dr_l = 1 - dr_u
-            sum_1_bg[i] = dr_u*sum_1_bg_grid_l + dr_l*sum_1_bg_grid_u
-
-
-@njit_serial()
-def gather_dr_psi_bg(sum_2_bg_grid, r_min, r_max, dr, r, sum_2_bg):
-    """
-    Convenient method for gathering at once the three source fields needed
-    by the Baxevanis wakefield model (a2 and nabla_a from the laser, and
-    b_theta_0 from the beam) into the plasma particles. This method is also
-    catered to cylindrical geometry and assumes all plasma particles have the
-    same longitudinal position (which is the case in a quasistatic model,
-    where there is only a single column of particles).
-
-    Parameters
-    ----------
-    fld_1, fld_2, fld_3 : ndarray
-        The three source fields, corresponding respectively to a2, nabla_a2
-        and b_theta_0. Each of them is a (nr+4) array, including 2 guard
-        cells in each boundary.
-    z_min, z_max : float
-        Position of the first and last field values along z.
-    r_min, r_max : float
-        Position of the first and last field values along r.
-    dz, dr : float
-        Grid step size along the longitudinal and radial direction.
-    r : 1darray
-        Transverse position of the plasma particles.
-    z : int
-        Longitudinal position of the column of plasma particles.
-
-    Returns
-    --------
-    A tuple with three 1darray containing the gathered field values at the
-    position of each particle.
-
-    """
-
-    # Iterate over all particles.
-    for i in range(r.shape[0]):
-
-        # Get particle position.
-        r_i = r[i]
-
-        # Gather field only if particle is within field boundaries.
-        if r_i <= r_max:
-            # Position in cell units.
-            r_i_cell = (r_i - r_min)/dr + 2
+            r_i_cell = (r_i - r_min) / dr + 2
 
             # Indices of upper and lower cells in r and z.
             ir_lower = int(math.floor(r_i_cell))
@@ -190,10 +118,12 @@ def gather_dr_psi_bg(sum_2_bg_grid, r_min, r_max, dr, r, sum_2_bg):
                 sign = -1
 
             # Get field value at each bounding cell.
-            sum_2_bg_grid_l = sum_2_bg_grid[ir_lower] * sign
-            sum_2_bg_grid_u = sum_2_bg_grid[ir_upper]
+            fld_l = b_t[ir_lower] * sign
+            fld_u = b_t[ir_upper]
 
-            # Interpolate in r.
+            # Interpolate in r
             dr_u = ir_upper - r_i_cell
             dr_l = 1 - dr_u
-            sum_2_bg[i] = dr_u*sum_2_bg_grid_l + dr_l*sum_2_bg_grid_u
+            b_t_pp[i] += dr_u*fld_l + dr_l*fld_u
+        else:
+            b_t_pp[i] += b_t[-3] * r_max / r_i
