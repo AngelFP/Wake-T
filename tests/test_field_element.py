02dee240f957cda11b30ba39bbc8d50097078b0e
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-09-12 18:35:32 +0200

    Add test

--- tests/test_field_element.py
+++ tests/test_field_element.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 import scipy.constants as ct
-from pytest import approx
+from pytest import approx, raises
 
 from wake_t.utilities.bunch_generation import get_gaussian_bunch_from_twiss
 from wake_t.fields.analytical_field import AnalyticalField
@@ -69,5 +69,40 @@ def test_field_element_tracking():
     assert approx(beta_x, rel=1e-10) == 0.054508554263608434
 
 
+def test_field_element_error():
+    """Test that the expected errors are raised"""
+    output_folder = os.path.join(
+        tests_output_folder, 'test_field_element_tracking')
+
+    # Create bunch.
+    emitt_nx = emitt_ny = 1e-6  # m
+    beta_x = beta_y = 1.  # m
+    s_t = 100.  # fs
+    gamma_avg = 1000
+    ene_spread = 0.1  # %
+    q_bunch = 30  # pC
+    xi_avg = 0.  # m
+    n_part = 1e4
+    bunch = get_gaussian_bunch_from_twiss(
+        en_x=emitt_nx, en_y=emitt_ny, a_x=0, a_y=0, b_x=beta_x, b_y=beta_y,
+        ene=gamma_avg, ene_sp=ene_spread, s_t=s_t, xi_c=xi_avg,
+        q_tot=q_bunch, n_part=n_part, name='elec_bunch')
+
+    # Create field element with too many dt_bunch.
+    element = FieldElement(
+        length=1,
+        dt_bunch=[1e-3/ct.c, 1e-3/ct.c],
+        n_out=10,
+        fields=[],
+    )
+
+    # Check that an error is raised because the number of `dt_bunch` does
+    # not agree with the number of bunches.
+    diag_dir = os.path.join(output_folder, 'diags')
+    with raises(ValueError) as e_info:
+        element.track(bunch, opmd_diag=True, diag_dir=diag_dir)
+
+
 if __name__ == '__main__':
     test_field_element_tracking()
+    test_field_element_error()
