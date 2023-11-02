3d80509816dcce7234ef485adf5e8a81bcb22a97
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-11-02 14:45:17 +0100

    Calculate bunch source also on guard cells

--- wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/b_theta_bunch.py
+++ wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/b_theta_bunch.py
@@ -7,7 +7,7 @@ from wake_t.utilities.numba import njit_serial
 
 
 @njit_serial()
-def calculate_bunch_source(q_bunch, n_r, n_xi, r_grid, dr, b_t_bunch):
+def calculate_bunch_source(q_bunch, n_r, n_xi, b_t_bunch):
     """
     Calculate the bunch source term (azimuthal magnetic field) from the
     charge deposited on the grid.
@@ -20,30 +20,29 @@ def calculate_bunch_source(q_bunch, n_r, n_xi, r_grid, dr, b_t_bunch):
         Number of grid points along r and xi.
     r_grid : float
         Radial coordinated of the grid points.
-    dr : float
-        Radial grid spacing.
     b_t_bunch : ndarray
         A (nz+4, nr+4) array where the magnetic field will be stored.
     """
-    inv_r = 1. / r_grid
     for i in range(n_xi):
         cumsum = 0.
-        for j in range(n_r):
+        # Iterate until n_r + 2 to get bunch source also in guard cells.
+        for j in range(n_r + 2):
             q_ij = q_bunch[2 + i, 2 + j]
             cumsum += q_ij
             # At each grid cell, calculate integral only until cell center by
             # assuming that half the charge is evenly distributed within the
             # cell (i.e., subtract half the charge)
-            b_t_bunch[2 + i, 2 + j] = (cumsum - 0.5 * q_ij) * dr * inv_r[j]
+            b_t_bunch[2 + i, 2 + j] = (cumsum - 0.5 * q_ij) * 1. / (0.5 + j)
         # At the first grid point along r, subtract an additional 1/4 of the
         # charge. This comes from assuming that the density has to be zero on
         # axis.
-        b_t_bunch[2 + i, 2] -= 0.25 * q_bunch[2 + i, 2] * dr * inv_r[0]
+        b_t_bunch[2 + i, 2] -= 0.25 * q_bunch[2 + i, 2] * 2.
 
 
 @njit_serial()
 def deposit_bunch_charge(
-    x, y, z, q, n_p, n_r, n_xi, r_grid, xi_grid, dr, dxi, p_shape, q_bunch
+    x, y, z, q, n_p, n_r, n_xi, r_grid, xi_grid, dr, dxi, p_shape, q_bunch,
+    r_min_deposit=0.
 ):
     """
     Deposit the charge of particle bunch in a 2D grid.
@@ -64,6 +63,14 @@ def deposit_bunch_charge(
         Particle shape. Used to deposit the charge on the grid.
     q_bunch : ndarray
         A (nz+4, nr+4) array where the charge will be deposited.
+    r_min_deposit : float
+        The minimum radial position that particles must have in order to
+        deposit their weight on the grid.
+
+    Returns
+    -------
+    bool
+        Whether all particles managed to deposit on the grid.
     """
     n_part = x.shape[0]
     s_d = ct.c / np.sqrt(ct.e**2 * n_p / (ct.m_e*ct.epsilon_0))
@@ -72,6 +79,7 @@ def deposit_bunch_charge(
     for i in range(n_part):
         w[i] = q[i] * k
 
-    deposit_3d_distribution(z, x, y, w, xi_grid[0], r_grid[0], n_xi,
-                            n_r, dxi, dr, q_bunch, p_shape=p_shape,
-                            use_ruyten=True)
+    return deposit_3d_distribution(
+        z, x, y, w, xi_grid[0], r_grid[0], n_xi,
+        n_r, dxi, dr, q_bunch, p_shape=p_shape,
+        use_ruyten=True, r_min_deposit=r_min_deposit)
