b4f14f42cb98dc46168b09b94d1b469f02eaa87b
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-11-02 14:37:37 +0100

    Improve 3d deposition methods for adaptive grids
    - Add `r_min_deposit` to prevent particles with smaller radii to
    deposit. This is useful when they have to deposit in several grids.
    - The methods now return a bool that determines whethere all
    particles managed to deposit.
    - The particles are now allowed to deposit in the guard cells at higher
    radii.

--- wake_t/particles/deposition.py
+++ wake_t/particles/deposition.py
@@ -16,7 +16,7 @@ from wake_t.utilities.numba import njit_serial, prange
 @njit_serial()
 def deposit_3d_distribution(z, x, y, w, z_min, r_min, nz, nr, dz, dr,
                             deposition_array, p_shape='cubic',
-                            use_ruyten=False):
+                            use_ruyten=False, r_min_deposit=0.):
     """
     Deposit the the weight of each particle of a 3D distribution into a 2D
     grid (cylindrical symmetry).
@@ -44,16 +44,24 @@ def deposit_3d_distribution(z, x, y, w, z_min, r_min, nz, nr, dz, dr,
         modified within this function)
     p_shape : str
         Particle shape to be used. Possible values are 'linear' or 'cubic'.
+    r_min_deposit : float
+        The minimum radial position that particles must have in order to
+        deposit their weight on the grid.
+
+    Returns
+    -------
+    bool
+        Whether all particles managed to deposit on the grid.
 
     """
     if p_shape == 'linear':
         return deposit_3d_distribution_linear(
             z, x, y, w, z_min, r_min, nz, nr, dz, dr, deposition_array,
-            use_ruyten)
+            use_ruyten, r_min_deposit)
     elif p_shape == 'cubic':
         return deposit_3d_distribution_cubic(
             z, x, y, w, z_min, r_min, nz, nr, dz, dr, deposition_array,
-            use_ruyten)
+            use_ruyten, r_min_deposit)
     else:
         raise ValueError(
             "Particle shape not recognized. "
@@ -63,7 +71,8 @@ def deposit_3d_distribution(z, x, y, w, z_min, r_min, nz, nr, dz, dr,
 
 @njit_serial
 def deposit_3d_distribution_linear(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
-                                   deposition_array, use_ruyten=False):
+                                   deposition_array, use_ruyten=False,
+                                   r_min_deposit=0.):
     """ Calculate charge distribution assuming linear particle shape. """
 
     # Precalculate particle shape coefficients needed to satisfy charge
@@ -84,6 +93,8 @@ def deposit_3d_distribution_linear(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
     z_max = z_min + (nz - 1) * dz
     r_max = nr * dr
 
+    all_deposited = True
+
     # Loop over particles.
     for i in prange(z.shape[0]):
         # Get particle components.
@@ -96,7 +107,12 @@ def deposit_3d_distribution_linear(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
         r_i = math.sqrt(x_i ** 2 + y_i ** 2)
 
         # Deposit only if particle is within field boundaries.
-        if z_i >= z_min and z_i <= z_max and r_i <= r_max:
+        if (
+            z_i >= z_min
+            and z_i <= z_max
+            and r_i >= r_min_deposit
+            and r_i <= r_max
+        ):
             # Positions of the particles in cell units.
             r_cell = (r_i - r_min) / dr
             z_cell = (z_i - z_min) / dz
@@ -109,9 +125,6 @@ def deposit_3d_distribution_linear(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
             if r_cell < 0:
                 # Force all charge to be deposited above axis.
                 u_r = 1.
-            elif r_cell > nr - 1:
-                # Force all charge to be deposited below r_max.
-                u_r = 0.
             else:
                 u_r = r_cell - int(math.ceil(r_cell)) + 1
 
@@ -145,12 +158,15 @@ def deposit_3d_distribution_linear(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
             deposition_array[iz_cell + 1, ir_cell + 0] += zsl_1 * rsl_0 * w_i
             deposition_array[iz_cell + 1, ir_cell + 1] += zsl_1 * rsl_1 * w_i
 
-    return
+        else:
+            all_deposited = False
+    return all_deposited
 
 
 @njit_serial
 def deposit_3d_distribution_cubic(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
-                                  deposition_array, use_ruyten=False):
+                                  deposition_array, use_ruyten=False,
+                                  r_min_deposit=0.):
     """ Calculate charge distribution assuming cubic particle shape. """
 
     # Precalculate particle shape coefficients needed to satisfy charge
@@ -172,6 +188,8 @@ def deposit_3d_distribution_cubic(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
     z_max = z_min + (nz - 1) * dz
     r_max = nr * dr
 
+    all_deposited = True
+
     # Loop over particles.
     for i in prange(z.shape[0]):
         # Get particle components.
@@ -184,7 +202,12 @@ def deposit_3d_distribution_cubic(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
         r_i = math.sqrt(x_i ** 2 + y_i ** 2)
 
         # Deposit only if particle is within field boundaries.
-        if z_i >= z_min and z_i <= z_max and r_i <= r_max:
+        if (
+            z_i >= z_min
+            and z_i <= z_max
+            and r_i >= r_min_deposit
+            and r_i <= r_max
+        ):
             # Positions of the particle in cell units.
             r_cell = (r_i - r_min) / dr
             z_cell = (z_i - z_min) / dz
@@ -230,15 +253,6 @@ def deposit_3d_distribution_cubic(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
             elif r_cell <= 1.:
                 rsc_1 += rsc_0
                 rsc_0 = 0.
-            # Above r_max:
-            elif r_cell > nr - 1:
-                rsc_0 += rsc_3
-                rsc_1 += rsc_2
-                rsc_2 = 0.
-                rsc_3 = 0.
-            elif r_cell > nr - 2:
-                rsc_2 += rsc_3
-                rsc_3 = 0.
             # Below z_min:
             if z_cell <= 0.:
                 zsc_3 += zsc_0
@@ -276,4 +290,6 @@ def deposit_3d_distribution_cubic(z, x, y, q, z_min, r_min, nz, nr, dz, dr,
             deposition_array[iz_cell + 3, ir_cell + 2] += zsc_3 * rsc_2 * w_i
             deposition_array[iz_cell + 3, ir_cell + 3] += zsc_3 * rsc_3 * w_i
 
-    return
+        else:
+            all_deposited = False
+    return all_deposited
