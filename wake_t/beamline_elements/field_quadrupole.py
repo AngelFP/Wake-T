1f2d7380a39c17511675a78c686d8e7ca0ae833e
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2024-05-03 10:32:17 +0200

    Enable automatic time step in `FieldQuadrupole`

--- wake_t/beamline_elements/field_quadrupole.py
+++ wake_t/beamline_elements/field_quadrupole.py
@@ -1,4 +1,9 @@
-from typing import List, Literal, Union, Optional
+from typing import Literal,  Optional
+
+import numpy as np
+import scipy.constants as ct
+
+from .plasma_stage import DtBunchType
 from .field_element import FieldElement
 from wake_t.physics_models.em_fields.quadrupole import QuadrupoleField
 
@@ -15,8 +20,13 @@ class FieldQuadrupole(FieldElement):
         Focusing strength of the quadrupole in :math:`T/m`. Defined so
         that a positive value is focusing for electrons in the :math:`y`
         plane and defocusing in the :math:`x` plane.
-    dt_bunch : float | str | List[float  |  str]
-        The time step for evolving the particle bunches.
+    dt_bunch : float, str, or list of float and str
+        The time step for evolving the particle bunches. If ``'auto'``, it will
+        be automatically set to :math:`dt = T/(10*2*pi)`, where T is the
+        betatron period of the particle with the lowest energy in the bunch.
+        A list of values can also be provided. In this case, the list
+        should have the same order as the list of bunches given to the
+        ``track`` method.
     bunch_pusher : str
         The pusher used to evolve the particle bunches in time within
         the specified fields. Possible values are ``'rk4'`` (Runge-Kutta
@@ -34,7 +44,7 @@ class FieldQuadrupole(FieldElement):
         self,
         length: float,
         foc_strength: float,
-        dt_bunch: Union[float, str, List[Union[float, str]]],
+        dt_bunch: Optional[DtBunchType] = 'auto',
         bunch_pusher: Literal['boris', 'rk4'] = 'boris',
         n_out: Optional[int] = 1,
         name: Optional[str] = 'quadrupole',
@@ -47,5 +57,15 @@ class FieldQuadrupole(FieldElement):
             n_out=n_out,
             name=name,
             fields=[QuadrupoleField(foc_strength)],
-            auto_dt_bunch=None,
+            auto_dt_bunch=self._get_optimized_dt,
         )
+
+    def _get_optimized_dt(self, beam):
+        """ Get tracking time step. """
+        # Get minimum gamma in the bunch (assumes px,py << pz).
+        q_over_m = beam.q_species / beam.m_species
+        min_gamma = np.sqrt(np.min(beam.pz)**2 + 1)
+        w_x = np.sqrt(np.abs(q_over_m*ct.c * self.foc_strength/min_gamma))
+        T_x = 1/w_x
+        dt = 0.1*T_x
+        return dt
